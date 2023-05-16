b5fc3a4245f72a1865e2fa416a749f28e07c761e
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-16 14:52:44 +0000

    Added CrudService interface. Closes #11

---
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,15 @@
+package com.alexquazar.sfgpetclinic.services;
+
+import java.util.Set;
+
+public interface CrudService<T, ID> {
+    Set<T> findAll();
+
+    T FindById(ID id);
+
+    T save(T object);
+
+    void delete(T object);
+
+    void deleteById(ID id);
+}
--- sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/services/OwnerService.java
@@ -1,16 +1,9 @@
 package com.alexquazar.sfgpetclinic.services;
 
-import java.util.Set;
-
 import com.alexquazar.sfgpetclinic.model.Owner;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     
     Owner findByLastName(String lastName);
 
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
