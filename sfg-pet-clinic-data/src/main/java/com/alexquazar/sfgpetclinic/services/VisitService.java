73d84904a161fae22015c29136d94936481e161a
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-30 20:16:09 +0000

    Added Map imp for Visit Service. Closes #46

---
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.alexquazar.sfgpetclinic.services;
+
+import com.alexquazar.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+    
+}
---
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package com.alexquazar.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import com.alexquazar.sfgpetclinic.model.Visit;
+import com.alexquazar.sfgpetclinic.services.VisitService;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        } else {
+            return super.save(visit);
+        }
+    }
+
+}
