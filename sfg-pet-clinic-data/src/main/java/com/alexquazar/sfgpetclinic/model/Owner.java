90d31871b699d05cd8e4931739f53f5d28ff7d44
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-05-30 15:11:16 +0000

    Updated owner with JPA entity mappings. Closes #35

--- sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/model/Owner.java
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/model/Owner.java
@@ -3,11 +3,26 @@ package com.alexquazar.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/model/Pet.java
+++ sfg-pet-clinic-data/src/main/java/com/alexquazar/sfgpetclinic/model/Pet.java
@@ -2,9 +2,29 @@ package com.alexquazar.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private PetType petType;
+    
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
 
     public String getName() {
         return name;
@@ -13,10 +33,6 @@ public class Pet extends BaseEntity {
     public void setName(String name) {
         this.name = name;
     }
-
-    private PetType petType;
-    private Owner owner;
-    private LocalDate birthDate;
 
     public PetType getPetType() {
         return petType;
