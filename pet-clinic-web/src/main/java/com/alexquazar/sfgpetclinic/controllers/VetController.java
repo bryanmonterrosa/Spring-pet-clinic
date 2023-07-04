9e8de3074daa03ec6332215003b18ec484c9e370
bryanmonterrosa <bryan_monterrosa@hotmail.com>
2023-07-04 20:53:24 +0000

    Create a JSON endpoint to list vets.

--- pet-clinic-web/src/main/java/com/alexquazar/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/alexquazar/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.alexquazar.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alexquazar.sfgpetclinic.model.Vet;
 import com.alexquazar.sfgpetclinic.services.VetService;
 
 @Controller
@@ -19,5 +23,10 @@ public class VetController {
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
